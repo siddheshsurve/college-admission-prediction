d9b1f2baabee5580efee4bc0d9b00cf54815f1e1
Samarth Tandale <tandalesamarth@gmail.com>
2024-02-13 14:30:11 +0530

    Sam's push :)

--- contact.py
+++ contact.py
@@ -3,5 +3,5 @@ import pandas as pd
 def app() :
     st.title("Contact Us")
 
-    st.text('Call us on : 8390344586 / 9850365380')
-    st.text('Mail us at : sysurve24@gmail.com / srtekade2797@gmail.com')
+    st.text('Call us on : 8390344586 / 9850365380 / 9730448100')
+    st.text('Mail us at : sysurve24@gmail.com / srtekade2797@gmail.com / tandalesamarth@gmail.com')
