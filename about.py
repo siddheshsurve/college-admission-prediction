6a51540d5fec2f3340dc9182072daecfa8fc3986
siddheshsurve <sysurve24@gmail.com>
2024-03-31 10:11:24 +0530

    added about

---
+++ about.py
@@ -0,0 +1,9 @@
+import streamlit as st 
+import pandas as pd
+def app() :
+    st.title("About us")
+
+    st.text('Call us on : 8390344586 / 9850365380 / 9730448100')
+    st.text('Mail us at : sysurve24@gmail.com / srtekade2797@gmail.com / tandalesamarth@gmail.com')
+
+
